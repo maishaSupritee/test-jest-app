69f7f4702f43807fc775f4229cbb6678c2c70eec
maishaSupritee <maisha.sup@gmail.com>
2025-07-17 13:02:27 -0700

    configure jest for typescript
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    // Handle module aliases
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}",
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+  ],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+    "reportWebVitals.ts",
+    "setup.ts",
+    "index.tsx",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
